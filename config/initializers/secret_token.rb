4a22a8ccdd38dbcf0fb2299b8f7c04f6b2274b85
Jonathan Chan <jonchan6@gmail.com>
2014-01-23 22:32:08 -0800

    added rspec

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,18 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
+require 'securerandom'
+
+def secure_token
+  token_file = Rails.root.join('.secret')
+  if File.exist?(token_file)
+    # Use the existing token.
+    File.read(token_file).chomp
+  else
+    # Generate a new token and store it in token_file.
+    token = SecureRandom.hex(64)
+    File.write(token_file, token)
+    token
+  end
+end
 Blog::Application.config.secret_key_base = '637a28140f801ee286d8254807c83dff5644a0e161586809b274f455aab827ecdae57fef18a311be306a9ec9ba899c8f61808e7a83a659e47dd735ab5541327a'
